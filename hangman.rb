a3f7dce3ff42869465952ca65493492260cb13ad
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-08-17 17:57:54 -0400

    roughed out guess validation method

--- hangman.rb
+++ hangman.rb
@@ -4,9 +4,29 @@ def choose_word file
   File.readlines(file).sample
 end
 
+def get_guess 
+  print "Guess a letter or a word: "
+  guess = gets.chomp
+  if valid_guess guess
+    return guess
+  else
+    puts "Invalid input."
+    get_guess 
+  end
+end
+
+def valid_guess
+  # checks to see whether
+    # guess includes non-letter characters
+      # if it includes non-letter characters, gives error message ("Sorry, guesses should be letters only!") and re-requests guess
+    # guess has already been guessed
+      # if already guessed, gives error message ("You already guessed that!") and re-requests guess
+    # guess is a letter or a word
+      # if letter, returns :letter
+      # if word, returns :word
+end
 
 wordfile = '1000words.txt'
 
 secret_word = choose_word wordfile
 
-puts secret_word
