748723decdd0e815383b70b3bf190217c59b9a20
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-08-17 17:45:38 -0400

    created secret word choosing method

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 
-puts "Program goes here..."
+def choose_word file
+  File.readlines(file).sample
+end
+
+
+wordfile = '1000words.txt'
+
+secret_word = choose_word wordfile
+
+puts secret_word
